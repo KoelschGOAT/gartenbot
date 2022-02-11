9ec13f8ecfc5d3e4187e1c696742a154dc857e1d
Luca Düpmann <luca.duepmann@gmail.com>
2022-02-11 12:57:38 +0100

    chart test

--- Client/gartenbot/src/Components/Sensor.js
+++ Client/gartenbot/src/Components/Sensor.js
@@ -1,5 +1,27 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 import React, { useEffect, useState } from "react";
-
+ChartJS.register(ArcElement, Tooltip, Legend);
+export const data = {
+  labels: ['Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19 ],
+      backgroundColor: [
+        
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+       
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
 function Sensor() {
   const [sensor, setSensor] = useState([]);
   useEffect(() => {
@@ -7,7 +29,10 @@ function Sensor() {
       .then((res) => res.json())
       .then((data) => setSensor(data));
   }, []);
-  return <div>{JSON.stringify(sensor, 2, null)}</div>;
+  return <><div>{JSON.stringify(sensor, 2, null)}</div>
+ 
+
+<Pie data = {data} /></>
 }
 
 export default Sensor;
